d3aa9bf23543b8b8f7baf928c2a80673bcdf65c8
Ansh Zamde <ajaybalazamde@gmail.com>
2026-08-20 13:21:35 +0530

    added React and vite components

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,16 @@
+
+import youtube from "./youtube.jsx"
+
+function App() {
+
+
+  return (
+      <>
+        <h1>vite react app</h1>
+        <h1>vite react app</h1>
+
+      </>
+  )
+}
+
+export default App
