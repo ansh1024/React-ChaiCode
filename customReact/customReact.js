d2b14877b09f3dd9da07c4d62e081025cd4eae69
Ansh Zamde <ajaybalazamde@gmail.com>
2026-08-20 21:20:58 +0530

    some more progress

---
+++ customReact/customReact.js
@@ -0,0 +1,20 @@
+function mainContainer(){
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+    container.appendChild(domElement)
+
+}
+
+const reactElement={
+    type : 'a'
+    props : {
+        href : "https://google.com"
+        target: "_blank",
+    },
+    children : 'Click to visit google'
+}
+
+const maincontainer= document.querySelector('#root')
+customRender(reactElement, maincontainer)
