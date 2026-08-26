78fcf9cbe722ad33b7624140246097bbb72f9c95
Ansh Zamde <ajaybalazamde@gmail.com>
2026-08-26 12:29:29 +0530

    another project

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,122 +1,128 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
+import { useState, useEffect, useCallback, useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [length, setLength] = useState(8)
+    const [numberAllowed, setNumberAllowed] = useState(false)
+    const [charAllowed, setCharAllowed] = useState(false)
+    const [password, setPassword] = useState('')
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    const passwordRef = useRef(null)
 
-      <div className="ticks"></div>
+    const passwordGenerator = useCallback(() => {
+        let pass = ''
+        let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        if (numberAllowed) {
+            str += '0123456789'
+        }
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+        if (charAllowed) {
+            str += '!@#$%^&*()_+-=[]{}|;:,.<>?'
+        }
+
+        for (let i = 1; i <= length; i++) {
+            let char = Math.floor(Math.random() * str.length)
+            pass += str.charAt(char)
+        }
+
+        setPassword(pass)
+    }, [length, numberAllowed, charAllowed])
+
+    const copyPasswordToClipboard = useCallback(() => {
+        passwordRef.current?.select()
+        window.navigator.clipboard.writeText(password)
+    }, [password])
+
+    useEffect(() => {
+        passwordGenerator()
+    }, [length, numberAllowed, charAllowed, passwordGenerator])
+
+    return (
+        <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+
+            <h1 className="text-white text-center my-3">
+                Password Generator
+            </h1>
+
+            {/* Password input + Copy button */}
+            <div className="flex shadow rounded-lg overflow-hidden mb-4">
+
+                <input
+                    type="text"
+                    value={password}
+                    className="outline-none w-full py-1 px-3"
+                    placeholder="Password"
+                    readOnly
+                    ref={passwordRef}
+                />
+
+                <button
+                    onClick={copyPasswordToClipboard}
+                    className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+                >
+                    Copy
+                </button>
+
+            </div>
+
+            {/* Options */}
+            <div className="flex items-center gap-x-2">
+
+                {/* Length */}
+                <input
+                    type="range"
+                    min={6}
+                    max={100}
+                    value={length}
+                    className="cursor-pointer"
+                    onChange={(e) => setLength(Number(e.target.value))}
+                />
+
+                <label>
+                    Length: {length}
+                </label>
+
+                {/* Numbers */}
+                <div className="flex items-center gap-x-1">
+
+                    <input
+                        type="checkbox"
+                        checked={numberAllowed}
+                        onChange={() => {
+                            setNumberAllowed((prev) => !prev)
+                        }}
+                        id="number"
+                    />
+
+                    <label htmlFor="number">
+                        Numbers
+                    </label>
+
+                </div>
+
+                {/* Characters */}
+                <div className="flex items-center gap-x-1">
+
+                    <input
+                        type="checkbox"
+                        checked={charAllowed}
+                        onChange={() => {
+                            setCharAllowed((prev) => !prev)
+                        }}
+                        id="character"
+                    />
+
+                    <label htmlFor="character">
+                        Character
+                    </label>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
 }
 
 export default App
